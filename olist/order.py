efeaf660d039549857d1a849c980cb1746b549e2
Rafael Camoes <rafaelcamoes@gmail.com>
2023-02-28 22:13:14 -0300

    added get_wait_time function

--- olist/order.py
+++ olist/order.py
@@ -2,6 +2,10 @@ import pandas as pd
 import numpy as np
 from olist.utils import haversine_distance
 from olist.data import Olist
+olist = Olist()
+data = olist.get_data()
+orders = data['orders'].copy() # good practice to be sure not to modify your `data` variable
+assert(orders.shape == (99441, 8))
 
 
 class Order:
@@ -13,14 +17,27 @@ class Order:
         # Assign an attribute ".data" to all new instances of Order
         self.data = Olist().get_data()
 
+
     def get_wait_time(self, is_delivered=True):
         """
-        Returns a DataFrame with:
-        [order_id, wait_time, expected_wait_time, delay_vs_expected, order_status]
-        and filters out non-delivered orders unless specified
+        Returns the wait time for a given order status.
         """
-        # Hint: Within this instance method, you have access to the instance of the class Order in the variable self, as well as all its attributes
-        pass  # YOUR CODE HERE
+        delivered_orders = orders[orders.order_status == 'delivered'].copy()
+        delivered_orders.loc[:, 'order_purchase_timestamp'] = delivered_orders.loc[:, 'order_purchase_timestamp'].apply(pd.to_datetime)
+        delivered_orders.loc[:,['order_purchase_timestamp', 'order_approved_at', 'order_delivered_carrier_date','order_delivered_customer_date', 'order_estimated_delivery_date', ]] = delivered_orders.loc[:,['order_purchase_timestamp', 'order_approved_at', 'order_delivered_carrier_date','order_delivered_customer_date', 'order_estimated_delivery_date', ]].apply(pd.to_datetime)
+        #convert dates from "string" type to "pandas.datetime' using pandas.to_datetime()
+        delivered_orders.loc[:, 'order_purchase_timestamp'] = delivered_orders.loc[:, 'order_purchase_timestamp'].apply(pd.to_datetime)
+        delivered_orders.loc[:,['order_purchase_timestamp', 'order_approved_at', 'order_delivered_carrier_date','order_delivered_customer_date', 'order_estimated_delivery_date', ]] = delivered_orders.loc[:,['order_purchase_timestamp', 'order_approved_at', 'order_delivered_carrier_date','order_delivered_customer_date', 'order_estimated_delivery_date', ]].apply(pd.to_datetime)
+        # add wait_time column
+        delivered_orders.loc[:, 'wait_time'] = delivered_orders.order_delivered_customer_date - delivered_orders.order_purchase_timestamp
+        # Compute expected_wait_time using loc
+        delivered_orders.loc[:, 'expected_wait_time'] = delivered_orders.order_estimated_delivery_date - delivered_orders.order_purchase_timestamp
+        # Compute delay_vs_expected
+        delivered_orders.loc[:, 'delay_vs_expected'] = delivered_orders.order_delivered_customer_date - delivered_orders.order_estimated_delivery_date
+        #show only order_id, wait_time, expected_wait_time, delay_vs_expected, order_status
+        delivered_orders = delivered_orders[['order_id', 'wait_time', 'expected_wait_time', 'delay_vs_expected', 'order_status']]
+
+        return delivered_orders
 
     def get_review_score(self):
         """
